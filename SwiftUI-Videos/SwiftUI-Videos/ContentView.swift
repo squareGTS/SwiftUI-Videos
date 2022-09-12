06f07f04cc1d2c70f24327b877a2122d2b4977e2
Maxim Bekmetov <mbekmetov@ibs.ru>
2022-09-12 19:31:31 +0300

    lesson 44

--- SwiftUI-Videos/SwiftUI-Videos/ContentView.swift
+++ SwiftUI-Videos/SwiftUI-Videos/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    var videos: [Video] = []
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            List(videos) { video in
+                Image(video.imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 90.0)
+                    .cornerRadius(8)
+
+                Spacer().frame(width: 16)
+
+                VStack(alignment: .leading) {
+                    Text(video.title)
+                        .fontWeight(.semibold)
+                        .lineLimit(2)
+                        .minimumScaleFactor(0.75)
+                    Spacer().frame(height: 5)
+                    Text(video.uploadDate)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+
+                }
+            }.navigationTitle(Text("Sean's Videos"))
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(videos: VideoList.topTwelve)
     }
 }
