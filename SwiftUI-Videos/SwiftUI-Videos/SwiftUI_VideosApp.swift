75ed43142955ccbb5eb26753c8fca9af02dc2be9
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-09-12 18:10:04 +0300

    Add files via upload

---
+++ SwiftUI-Videos/SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by Maxim Bekmetov on 12.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
